e0f73651616db7d3ac58b39b6e157b9dffa3e6c0
mark-marshall <mark.marshallgp@gmail.com>
2019-03-19 12:32:38 +0000

    [MVP] Added get post by ID segment

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const postIdUrl = '/api/posts/:id';
 
 routes.use(express.json());
 
+// GET ALL POSTS
 routes.get(postUrl, async (req, res) => {
   try {
     const posts = await db.find();
@@ -17,6 +18,25 @@ routes.get(postUrl, async (req, res) => {
     res
       .status(500)
       .json({ error: 'The posts information could not be retrieved.' });
+  }
+});
+
+// GET SPECIFIC POSTS
+routes.get(postIdUrl, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await db.findById(id);
+    if (post.length > 0) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' });
   }
 });
 
