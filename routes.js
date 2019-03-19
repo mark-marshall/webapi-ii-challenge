0e1a52cf7ff9eae8ab21534eabb72d0f31ccfe9a
mark-marshall <mark.marshallgp@gmail.com>
2019-03-19 12:26:54 +0000

    [MVP] Added get all posts segment

--- routes.js
+++ routes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const routes = express.Router();
 const db = require('./data/db');
 
+const postUrl = '/api/posts';
+const postIdUrl = '/api/posts/:id';
+
 routes.use(express.json());
+
+routes.get(postUrl, async (req, res) => {
+  try {
+    const posts = await db.find();
+    if (posts) {
+      res.status(200).json(posts);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
+});
 
 module.exports = routes;
